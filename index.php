785cd9940230717d89d8945d8fbfb1b41595f184
SatyabrataD <dashsatyabrata1999@gmail.com>
2025-12-24 18:23:34 +0530

    first-commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file 
+ * 
+ * @package Satyacustom2026
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SatyaCustom2026</title>
+</head>
+<body>
+  <h1>satya</h1>
+</body>
+</html>
